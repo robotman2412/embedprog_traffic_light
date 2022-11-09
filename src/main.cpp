31dd35d9d2f8b41b36a8ce7cd8b909594e921b1f
RobotMan2412 <julian@scheffers.net>
2022-11-09 15:22:09 +0100

    Fix a thing

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #define BTN_RIGHT   4
 #define BTN_CENTER 15
 
+#define HAL_PIN 32
+
 Light lightA(13, LIGHT_YELLOW_EMU, 21);
 Light lightB(14, LIGHT_YELLOW_EMU, 12);
 
@@ -31,16 +33,15 @@ void setup() {
 	detB = Detector(0, BTN_RIGHT, FALLING);
 	
 	// The test SYSTEM.
-	phaseA = Phase("A", &lightA, &detA);
+	phaseA = Phase("A", &lightA);
 	phaseA.exclusive = 0x02;
 	phaseA.clearTime = 1000;
-	detA.phase = &phaseA;
+	phaseA.addDetector(detA);
 	
-	phaseB = Phase("A", &lightB, &detB);
+	phaseB = Phase("A", &lightB);
 	phaseB.exclusive = 0x01;
 	phaseB.clearTime = 1000;
-	detB.phase = &phaseB;
-	
+	phaseB.addDetector(detB);
 	
 	// Add phases to the SYSTEM.
 	trafficsys.phases.push_back(&phaseA);
@@ -55,8 +56,13 @@ void setup() {
 	// DEFAULT is A is GREEN.
 	trafficsys.defaultPhase = 0x01;
 	
+	pinMode(HAL_PIN, INPUT_PULLUP);
 }
 
 void loop() {
 	trafficsys.update();
+	
+	int val = analogRead(HAL_PIN);
+	printf("%3d\n", val);
+	delay(100);
 }
